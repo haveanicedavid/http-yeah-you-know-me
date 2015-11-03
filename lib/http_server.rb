d8c463eebdd6dfa252a1f0b6f3baab00a853b8cc
Emily Blanchard <emblou2@aol.com>
2015-11-03 15:07:08 -0700

    Got counter working

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -1,14 +1,55 @@
 require 'socket'
 require 'pry'
 
-class HTTP
+tcp_server = TCPServer.new(9292)
+counter = 0
 
-attr_accessor :port  
+loop do 
+client = tcp_server.accept
 
-def initialize(port)
- @port = port
- @server = TCPServer.new(port)
-end 
+puts "Ready for a request"
+request_lines = []
+while line = client.gets and !line.chomp.empty?
+  request_lines << line.chomp
+end
+
+counter +=1
+
+
+
+puts "Got this request:"
+puts request_lines.inspect
+
+puts "Sending response."
+response = "<pre> Hello World! (#{counter}) </pre>"
+output = "<html><head></head><body>#{response}</body></html>"
+headers = ["http/1.1 200 ok",
+          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+          "server: ruby",
+          "content-type: text/html; charset=iso-8859-1",
+          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+client.puts headers
+client.puts output
+
+puts ["Wrote this response:", headers, output].join("\n")
+client.close
+puts "\nResponse complete, exiting."
+
+end
+
+
+
+
+
+# class HTTP
+
+# attr_accessor :port  
+
+# def initialize(port)
+#  @port = port
+#  @server = TCPServer.new(port)
+# end 
+
 # end
 
 # # refresh_count = 0
--- test/http_server_test.rb
+++ test/http_server_test.rb
@@ -16,6 +16,9 @@ class HTTPTest < Minitest::Test
   end 
 
 
+# ["GET / HTTP/1.1", "Host: 127.0.0.1:9292", "Accept-Encoding: gzip, deflate", "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7", "Accept-Language: en-us", "Cache-Control: max-age=0", "Connection: keep-alive"]
+
+
 
  # def test_intialize_takes_argument
  #   data = Parser.new(request_lines)

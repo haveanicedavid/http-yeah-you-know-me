5712ba717b110263abad54714b2a64c8f44b10ea
Emily Blanchard <emblou2@aol.com>
2015-11-03 14:45:13 -0700

    No clue what we just did

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'pry'
 
 class HTTP
 
@@ -13,43 +14,47 @@ end
 
 def start 
  until server.closed? do 
-   client = server.accept
+   client = @server.accept
    parse(client)
-   # refresh_count += 1
  end 
-end
+end 
+
+# request_lines = []
+# # while line = client.gets and !line.chomp.empty?
+# #  request_lines << line.chomp
+# # end
+# puts "Got this request:"
+# puts request_lines.inspect
 
 def parse(client)
-request_lines = {} git
-while line = client.gets and !line.chomp.empty?
- request_lines << line.chomp
+  request_lines = Hash.new
+  first_line
+
 end
 
-puts "Got this request:"
-puts request_lines.inspect
 
-def response
 
-end 
-puts "Sending response."
-response = "<pre> Hello World! (#{refresh_count}) </pre>"
-output = "<html><head></head><body>#{response}</body></html>"
-headers = ["http/1.1 200 ok",
-         "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-         "server: ruby",
-         "content-type: text/html; charset=iso-8859-1",
-         "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-def headers
-end  
+# def response
 
-client.puts headers
-client.puts output
+# end 
+# puts "Sending response."
+# response = "<pre> Hello World! </pre>"
+# output = "<html><head></head><body>#{response}</body></html>"
+# headers = ["http/1.1 200 ok",
+#          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+#          "server: ruby",
+#          "content-type: text/html; charset=iso-8859-1",
+#          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+# def headers
+# end  
 
-# puts ["Wrote this response:", headers, output].join("\n")
+# client.puts headers
+# client.puts output
+
+# # puts ["Wrote this response:", headers, output].join("\n")
 
  def stop 
- client.close
+ server.close
  end
 
-end
 end

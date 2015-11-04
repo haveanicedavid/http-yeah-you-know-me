48f8dfebab3224502ff619b1d218a1f9b86de3e8
Steve Pentler <stevepentler@gmail.com>
2015-11-03 22:31:08 -0700

    Added pseudocode for iterationi 3 to http_server.rb

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -8,7 +8,6 @@ counter = 0
 
 loop do 
 client = tcp_server.accept
-
 puts "Ready for a request"
 request_lines = []
 while line = client.gets and !line.chomp.empty?
@@ -16,20 +15,31 @@ while line = client.gets and !line.chomp.empty?
 end
 
 counter +=1
-
-puts "Got this request:"
-puts request_lines.inspect
-
-# parser = Parser.new(request_lines)   
-# hash = parser.parse
-# path = Path.new
-# response = path.path_response
-
 parser = Parser.new(request_lines)
 hash = parser.parse
 datetime = Time.new.strftime('%l:%M%p on %A, %B %-d %Y')
 
+puts "Got this request:"
+puts request_lines.inspect
+
+#user inputs http address into url 
+#server recovers and stores code
+#make sure it enters as string
+#split at the question mark
+#take the 1st element of that split(the second half)
+#split the second half at the & symbol
+#take each element 
+#iterate through all the elements and split at the = symbol
+#take the first element or word fragment value
+#reference that word fragment value to the dictionary
+#if the word fragment value is in the dictionary
+  #output "#{word fragment value} is a known word
+#if the word fragment value is not in the dictionary
+  #output "#{word fragment value} is NOT a known word"
+
     case 
+    when hash[:Verb] == "GET" && hash[:Path] == "/"
+
     when hash[:Path] == "/"
       path_response = parser.formatted_parse
     when hash[:Path] == "/hello"
@@ -40,18 +50,15 @@ datetime = Time.new.strftime('%l:%M%p on %A, %B %-d %Y')
       path_response = "Total Requests (#{counter})"
       client.close
     end
-  
-
 
 puts "Sending response."
-# response = "<pre>Hello World! (#{counter}) \n#{formatted_parse} </pre>"
-# response = "<pre>#{selected_path} </pre>"
 output = "<html><head></head><body> #{path_response}</body></html>"
 headers = ["http/1.1 200 ok",
           "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
           "server: ruby",
           "content-type: text/html; charset=iso-8859-1",
           "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+
 client.puts headers
 client.puts output
 
@@ -59,59 +66,3 @@ puts ["Wrote this response:", headers, output].join("\n")
 client.close
 puts "\nResponse complete, exiting."
 end
-
-
-
-
-
-# class HTTP
-
-# attr_accessor :port  
-
-# def initialize(port)
-#  @port = port
-#  @server = TCPServer.new(port)
-# end 
-
-# end
-
-# # refresh_count = 0
-
-# def start 
-#  until server.closed? do 
-#    client = server.accept
-#    parse(client)
-#  end 
-# end 
-
-# # request_lines = []
-# # # while line = client.gets and !line.chomp.empty?
-# # #  request_lines << line.chomp
-# # # end
-# # puts "Got this request:"
-# # puts request_lines.inspect
-
-# # def response
-
-# # end 
-# # puts "Sending response."
-# # response = "<pre> Hello World! </pre>"
-# # output = "<html><head></head><body>#{response}</body></html>"
-# # headers = ["http/1.1 200 ok",
-# #          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-# #          "server: ruby",
-# #          "content-type: text/html; charset=iso-8859-1",
-# #          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-# # def headers
-# # end  
-
-# # client.puts headers
-# # client.puts output
-
-# # # puts ["Wrote this response:", headers, output].join("\n")
-
-#  def stop 
-#  server.close
-#  end
-
-# end

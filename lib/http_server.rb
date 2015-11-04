0aff4217da76fa44cf74a223d1ca5c4d6db8659c
Steve Pentler <stevepentler@gmail.com>
2015-11-03 18:23:58 -0700

    Created path file & class for select_path method

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -1,5 +1,6 @@
 require 'socket'
 require_relative 'parser'
+require_relative 'path'
 require 'pry'
 
 tcp_server = TCPServer.new(9292)
@@ -15,46 +16,6 @@ while line = client.gets and !line.chomp.empty?
 end
 counter +=1
 
-  # def parse(request_lines)
-  #   diagnostics = {}
-  #   diagnostics[:Verb] = request_lines[0].split[0]
-  #   diagnostics[:Path] = request_lines[0].split[1]
-  #   diagnostics[:Protocol] = request_lines[0].split[2]
-  #   diagnostics[:Host] = request_lines[1].split[1][1..-6]
-  #   diagnostics[:Port] = request_lines[1].split[1][-4..-1]
-  #   diagnostics[:Accept] = request_lines[3].split[1..-1]
-  #   debug = diagnostics.map do |key, value|
-  #     "#{key}: #{value}\n"
-  #   end
-  #   debug.join
-  # end
-
-  #   def select_path
-  #     #if path == '/'
-  #     if @hash[:Path] == '/'
-  #       #respond with parse(request_lines)
-  #       response = "<pre> #{parse(request_lines)} </pre>"
-  #       #respond with parse(request_lines)
-  #     elsif diagnostics[:Path] == '/hello'
-  # # # #   #respond with 'Hello World! AND Counter'
-  #       response = "<pre> Hello World! #{counter} </pre>" 
-  # #   #if root == '/datetime'
-  #     elsif diagnostics[:Path] == '/datetime'
-  #       binding.pry
-  # #   #respond with 11:07AM on Sunday, October November 1, 2015
-  #       response = Time.new.strftime('%l:%M%p on %A, %B %-d %Y')
-  # #   #if root == '/shutdown'
-  #     elsif diagnostics[:Path] == '/shutdown'
-  # #   #respond with "Total Requests: 12"
-  #       response = "Total Requests: 12"
-  # #     #12 equals aggregate of requests
-  #       client.close
-  # #       #server shuts down and exits 
-  #     end 
-  #     return response
-  #   end 
-  # end 
-
 puts "Got this request:"
 puts request_lines.inspect
 
@@ -63,7 +24,7 @@ formatted_parse = parse.formatted_parse
 
 
 puts "Sending response."
-# response = "<pre>Hello World! (#{counter}) \n#{formatted_parse} </pre>"
+response = "<pre>Hello World! (#{counter}) \n#{formatted_parse} </pre>"
 output = "<html><head></head><body> #{response}</body></html>"
 headers = ["http/1.1 200 ok",
           "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
@@ -76,7 +37,6 @@ client.puts output
 puts ["Wrote this response:", headers, output].join("\n")
 client.close
 puts "\nResponse complete, exiting."
-
 end
 
 
---
+++ lib/path.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+class Path
+
+  def select_path
+
+
+
+  end 
+end 
+
+
+
+
+
+
+
+
+
+  #   if @hash[:Path] == '/'
+      if path == '/'
+    # respond with parse(request_lines)
+        response = "<pre> #{parse(request_lines)} </pre>"
+  #       #respond with parse(request_lines)
+  #     elsif diagnostics[:Path] == '/hello'
+  # # # #   #respond with 'Hello World! AND Counter'
+  #       response = "<pre> Hello World! #{counter} </pre>" 
+  # #   #if root == '/datetime'
+  #     elsif diagnostics[:Path] == '/datetime'
+  #       binding.pry
+  # #   #respond with 11:07AM on Sunday, October November 1, 2015
+  #       response = Time.new.strftime('%l:%M%p on %A, %B %-d %Y')
+  # #   #if root == '/shutdown'
+  #     elsif diagnostics[:Path] == '/shutdown'
+  # #   #respond with "Total Requests: 12"
+  #       response = "Total Requests: 12"
+  # #     #12 equals aggregate of requests
+  #       client.close
+  # #       #server shuts down and exits 
+  #     end 
+  #     return response
+  #   end 
+  # end 
